cc2cc3d6c371f547b2d902f719c51a55744d8b30
James Ridgway <myself@james-ridgway.co.uk>
2017-10-25 21:57:50 +0100

    Improved tempremental test

--- src/test/java/uk/co/jamesridgway/gradle/gitflow/plugin/GitFlowPluginTest.java
+++ src/test/java/uk/co/jamesridgway/gradle/gitflow/plugin/GitFlowPluginTest.java
@@ -1,47 +1,69 @@
 package uk.co.jamesridgway.gradle.gitflow.plugin;
 
 import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.revwalk.RevCommit;
 import org.gradle.api.Project;
-import org.gradle.api.plugins.ExtensionContainer;
 import org.gradle.testfixtures.ProjectBuilder;
 import org.junit.Test;
-import uk.co.jamesridgway.gradle.gitflow.plugin.utils.Exceptions;
+import uk.co.jamesridgway.gradle.gitflow.plugin.version.UnknownVersion;
+import uk.co.jamesridgway.gradle.gitflow.plugin.version.UnreleasedVersion;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 
 public class GitFlowPluginTest {
 
     @Test
-    public void apply() {
-        ExtensionContainer extensionContainer = mock(ExtensionContainer.class);
-        Project mockProject = mock(Project.class);
-        when(mockProject.getExtensions()).thenReturn(extensionContainer);
-
-        GitFlowPlugin gitFlowPlugin = new GitFlowPlugin();
-        gitFlowPlugin.apply(mockProject);
-
-        verify(extensionContainer).create("gitflow", GitFlowPluginExtension.class, mockProject);
-        verify(mockProject).setVersion(any());
-    }
-
-    @Test
     public void buildAndApplyPlugin() throws Exception {
-        Project project = setupTestProject();
+        Project project = ProjectBuilder.builder().build();
+        Git.init().setDirectory(project.getRootDir()).call();
+        project.getPlugins().apply("uk.co.jamesridgway.gradle.gitflow.plugin");
 
         assertThat(project.getExtensions().getByName("gitflow"))
                 .isInstanceOf(GitFlowPluginExtension.class)
                 .isNotNull();
+
+        assertThat(project.getVersion())
+                .isEqualTo(new UnknownVersion());
     }
 
-    private Project setupTestProject() {
+    @Test
+    public void buildAndApplyPluginWithGitHistory() throws Exception {
         Project project = ProjectBuilder.builder().build();
-        Exceptions.propagateAnyError(() -> Git.init().setDirectory(project.getRootDir()).call());
+        Git git = Git.init().setDirectory(project.getRootDir()).call();
+
+        git.commit()
+                .setMessage("commit1")
+                .setAllowEmpty(true)
+                .call();
+
+        git.tag()
+                .setName("1.0.0")
+                .call();
+
+        git.branchCreate().setName("feature/james/FEAT-1").call();
+        git.checkout().setName("feature/james/FEAT-1").call();
+
+        git.commit()
+                .setMessage("commit2")
+                .setAllowEmpty(true)
+                .call();
+
+        RevCommit commit3 = git.commit()
+                .setMessage("commit3")
+                .setAllowEmpty(true)
+                .call();
+
+        String shortCommitId = commit3.getId().getName().substring(0, 7);
+
         project.getPlugins().apply("uk.co.jamesridgway.gradle.gitflow.plugin");
-        return project;
+
+        assertThat(project.getExtensions().getByName("gitflow"))
+                .isInstanceOf(GitFlowPluginExtension.class)
+                .isNotNull();
+
+        assertThat(project.getVersion())
+                .isInstanceOf(UnreleasedVersion.class)
+                .hasToString("1.0.0-feature/james/FEAT-1.2+sha." + shortCommitId);
     }
 
 }
