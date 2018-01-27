4d777c33dfb5710171638bf27296a3d989669079
James Ridgway <james.ridgway@thefloow.com>
2018-01-27 12:00:22 +0000

    Adding isRelease() to version and by default substitute  for  in branch names to add maven compatibility out of the box

--- src/test/java/uk/co/jamesridgway/gradle/gitflow/plugin/version/UnreleasedVersionTest.java
+++ src/test/java/uk/co/jamesridgway/gradle/gitflow/plugin/version/UnreleasedVersionTest.java
@@ -3,8 +3,6 @@ package uk.co.jamesridgway.gradle.gitflow.plugin.version;
 import org.junit.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
 public class UnreleasedVersionTest {
 
@@ -13,10 +11,7 @@ public class UnreleasedVersionTest {
         final String template = "${major}.${minor}.${patch}-${branch}.${commitsSinceLastTag}"
                 + "+sha.${commitId?substring(0,7)}${dirty?then('.dirty','')}";
 
-        ReleaseVersion releaseVersion = mock(ReleaseVersion.class);
-        when(releaseVersion.getMajor()).thenReturn(2);
-        when(releaseVersion.getMinor()).thenReturn(14);
-        when(releaseVersion.getPatch()).thenReturn("3");
+        ReleaseVersion releaseVersion = new ReleaseVersion(2, 14, 3);
 
         UnreleasedVersion unreleasedVersion = UnreleasedVersion.build(releaseVersion, template)
                 .withBranch("feature/test")
@@ -29,8 +24,18 @@ public class UnreleasedVersionTest {
         assertThat(unreleasedVersion.getMinor()).isEqualTo(14);
         assertThat(unreleasedVersion.getPatch()).isEqualTo("3");
         assertThat(unreleasedVersion.toString()).isEqualTo("2.14.3-feature/test.6+sha.a3dcb3e.dirty");
+    }
 
-
+    @Test
+    public void isNotRelease() {
+        ReleaseVersion releaseVersion = new ReleaseVersion(2, 14, 3);
+        UnreleasedVersion unreleasedVersion = UnreleasedVersion.build(releaseVersion, "")
+                .withBranch("feature/test")
+                .withCommitId("a3dcb3e")
+                .withCommitsSinceLastTag(6)
+                .withDirty(true)
+                .create();
+        assertThat(unreleasedVersion.isRelease()).isFalse();
     }
 
 }
